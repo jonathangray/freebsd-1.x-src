1ae201a503ae5f09d2b6f58ba52d790c897be029
nate <nate@FreeBSD.org>
1993-06-18 18:39:36 +0000

    Initial revision

---
+++ gnu/usr.bin/gzip/crypt.c
@@ -0,0 +1,6 @@
+/* crypt.c (dummy version) -- do not perform encryption
+ * Hardly worth copyrighting :-)
+ */
+#ifndef lint
+static char rcsid[] = "$Id: crypt.c,v 1.1 1993/06/18 18:39:37 nate Exp $";
+#endif
