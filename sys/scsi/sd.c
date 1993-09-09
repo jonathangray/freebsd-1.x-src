6293ae1197b7e99284c68ed8caff2d3a5eb6c8ad
rgrimes <rgrimes@FreeBSD.org>
1993-09-09 01:30:46 +0000

    Changed the delay from 100 seconds to 20 seconds if you don't have scsi dump code.

--- sys/scsi/sd.c
+++ sys/scsi/sd.c
@@ -14,7 +14,7 @@
  *
  * Ported to run under 386BSD by Julian Elischer (julian@dialix.oz.au) Sept 1992
  *
- *	$Id: sd.c,v 1.6 1993/08/28 03:08:56 rgrimes Exp $
+ *	$Id: sd.c,v 1.7 1993/09/09 01:30:46 rgrimes Exp $
  */
 
 #define SPLSD splbio
@@ -1638,7 +1638,7 @@ sddump(dev_t dev)			/* dump core after a system crash */
 sddump()
 {
 	printf("\nsddump()        -- not implemented\n");
-	DELAY(100000000);	/* 100 seconds */
+	DELAY(20000000);	/* 20 seconds */
 	return(-1);
 }
 #endif SCSIDUMP
