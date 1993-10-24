2a0dff6b301b15a120938a859927499c5cf50039
rgrimes <rgrimes@FreeBSD.org>
1993-10-24 04:30:08 +0000

    Change ISOFSMNT_NORRIP to just MNT_NORRIP to reflect the change in mount.h.
    Remove the #define ISOFS that was used to enable ISOFSMNT_NORRIP since
    this is now always defined under the new name MNT_NORRIP

--- sbin/mount_isofs/mount_isofs.c
+++ sbin/mount_isofs/mount_isofs.c
@@ -1,10 +1,9 @@
 #ifndef lint
-static char rcsid[] = "$Header: /a/cvs/386BSD/src/sbin/mount_isofs/mount_isofs.c,v 1.2 1993/07/20 03:33:49 jkh Exp $";
+static char rcsid[] = "$Header: /a/cvs/386BSD/src/sbin/mount_isofs/mount_isofs.c,v 1.3 1993/10/24 04:30:08 rgrimes Exp $";
 #endif
 
 #include <stdio.h>
 #include <sys/types.h>
-#define ISOFS
 #include <sys/mount.h>
 
 void
@@ -35,7 +34,7 @@ char **argv;
 			opts |= atoi(argv[0]);
 			argc--; argv++;
 		} else if (!strcmp(argv[0], "-norrip")) {
-			opts |= ISOFSMNT_NORRIP;
+			opts |= MNT_NORRIP;
 			argc--; argv++;
 		} else
 			usage();
