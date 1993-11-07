e8535c880538cb69a9bbd7ec640e59547209d4a8
wollman <wollman@FreeBSD.org>
1993-11-07 22:54:36 +0000

    Added the netrmp protocol from the Net/2 tape.  While of dubious utility, it's simple enough that it might actually work and be useful on some systems. (That's more than you can say for netccitt or netimp!)

--- sys/netinet/in.c
+++ sys/netinet/in.c
@@ -31,7 +31,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)in.c	7.17 (Berkeley) 4/20/91
- *	$Id: in.c,v 1.3 1993/11/07 17:47:49 wollman Exp $
+ *	$Id: in.c,v 1.4 1993/11/07 22:55:02 wollman Exp $
  */
 
 #include "param.h"
@@ -42,7 +42,6 @@
 #include "in_systm.h"
 #include "net/if.h"
 #include "net/route.h"
-#include "net/af.h"
 #include "in.h"
 #include "in_var.h"
 
