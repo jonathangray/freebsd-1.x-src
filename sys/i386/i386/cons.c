78d20a412faf769295b448b13659e940c77c3bce
davidg <davidg@FreeBSD.org>
1994-01-23 17:50:11 +0000

    Backed out previous commit as it requires additional kludges to work completely, and it looks like syscons might solve the problem a different way  (although what about serial consoles??).

--- sys/i386/i386/cons.c
+++ sys/i386/i386/cons.c
@@ -36,7 +36,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)cons.c	7.2 (Berkeley) 5/9/91
- *	$Id: cons.c,v 1.8 1994/01/22 22:48:21 davidg Exp $
+ *	$Id: cons.c,v 1.9 1994/01/23 17:50:11 davidg Exp $
  */
 
 
@@ -127,13 +127,10 @@ cnclose(dev, flag, mode, p)
 	int flag, mode;
 	struct proc *p;
 {
-	return(0);
-#if 0
 	if (cn_tab == NULL)
 		return (0);
 	dev = cn_tab->cn_dev;
 	return ((*cdevsw[major(dev)].d_close)(dev, flag, mode, p));
-#endif
 }
  
 int
