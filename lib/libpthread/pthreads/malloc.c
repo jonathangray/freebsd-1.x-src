ffe463803679bea160800c3b488aa7fa5e46aaab
proven <proven@FreeBSD.org>
1994-01-30 04:23:38 +0000

    Lots of new stuff in this release.
    Some more syscalls .....
    
    pthread_join, pthread_detach, sleep

--- lib/libpthread/pthreads/malloc.c
+++ lib/libpthread/pthreads/malloc.c
@@ -1,6 +1,6 @@
 /* ==== malloc.c ============================================================
  * Copyright (c) 1983 Regents of the University of California.
- * Copyright (c) 1993 by Chris Provenzano, proven@mit.edu
+ * Copyright (c) 1993, 1994 by Chris Provenzano, proven@mit.edu
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -44,11 +44,6 @@
  *      -Modified BSD libc malloc to be threadsafe.
  *
  */
-
-#if defined(LIBC_SCCS) && !defined(lint)
-/*static char *sccsid = "from: @(#)malloc.c     5.11 (Berkeley) 2/23/91";*/
-static char *rcsid = "$Id: malloc.c,v 1.3 1993/12/20 19:30:14 proven Exp $";
-#endif /* LIBC_SCCS and not lint */
 
 #include <pthread.h>
 #include <sys/types.h>
