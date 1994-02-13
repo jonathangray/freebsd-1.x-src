58a6ee93bcdf571a034d7fe6a9d6873fdf3efe06
jkh <jkh@FreeBSD.org>
1994-02-13 20:41:05 +0000

    This is Paul K's latest set of ld changes.  A commit was necessary at this late stage due to the fact that link.h was copyright Sun Microsystems.
    This version of ld sync's us up with NetBSD's ld and supports compatablily
    with NetBSD's -[zZ] flags (which we had reversed).  Compiling with this
    new ld will give you RRS warnings for libraries which do not contain .type
    infomation - these wsarnings are harmless and will go away as soon as you
    recompile your libraries (cd /usr/src; make libraries).

--- gnu/usr.bin/ld/i386/md-static-funcs.c
+++ gnu/usr.bin/ld/i386/md-static-funcs.c
@@ -1,8 +1,10 @@
-
 /*
+ *	$Id: md-static-funcs.c,v 1.2 1994/02/13 20:42:06 jkh Exp $
+ *
  * Called by ld.so when onanating.
  * This *must* be a static function, so it is not called through a jmpslot.
  */
+
 static void
 md_relocate_simple(r, relocation, addr)
 struct relocation_info	*r;
