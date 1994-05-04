3fcd496cee6edc03220e62ae6f7ef0da60d539ba
rgrimes <rgrimes@FreeBSD.org>
1994-05-04 08:50:12 +0000

    USL copyright

--- include/ctype.h
+++ include/ctype.h
@@ -1,4 +1,13 @@
 /*
+ * Copyright (c) UNIX System Laboratories, Inc.  All or some portions
+ * of this file are derived from material licensed to the
+ * University of California by American Telephone and Telegraph Co.
+ * or UNIX System Laboratories, Inc. and are reproduced herein with
+ * the permission of UNIX System Laboratories, Inc.
+ *
+ *	$Id: ctype.h,v 1.6 1994/05/04 08:50:12 rgrimes Exp $
+ */
+/*
  * Copyright (c) 1989, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
