4da820d54297ddfc93a3975d8ebd2ef5209c3751
phk <phk@FreeBSD.org>
1994-05-12 00:37:57 +0000

    made -v produce tip-revision, which was what I wanted in the first place...

--- gnu/usr.bin/rcs/rlog/rlog.c
+++ gnu/usr.bin/rcs/rlog/rlog.c
@@ -36,9 +36,12 @@ Report problems and direct all questions to:
 
 
 /* $Log: rlog.c,v $
-/* Revision 1.3  1994/05/11 22:39:44  phk
-/* Added -v option to rlog.  This gives a quick way to get a list of versions.
+/* Revision 1.4  1994/05/12 00:37:59  phk
+/* made -v produce tip-revision, which was what I wanted in the first place...
 /*
+ * Revision 1.3  1994/05/11  22:39:44  phk
+ * Added -v option to rlog.  This gives a quick way to get a list of versions.
+ *
  * Revision 1.2  1993/08/06  16:47:16  nate
  * Have rlog output be much easier to parse.  (Added one line which is not
  * used by any CVS/RCS commands)
@@ -201,7 +204,7 @@ static struct lockers *lockerlist;
 static struct stateattri *statelist;
 
 
-mainProg(rlogId, "rlog", "$Id: rlog.c,v 1.3 1994/05/11 22:39:44 phk Exp $")
+mainProg(rlogId, "rlog", "$Id: rlog.c,v 1.4 1994/05/12 00:37:59 phk Exp $")
 {
 	static char const cmdusage[] =
 		"\nrlog usage: rlog -{bhLRt} [-v[string]] -ddates -l[lockers] -rrevs -sstates -w[logins] -Vn file ...";
@@ -342,7 +345,8 @@ mainProg(rlogId, "rlog", "$Id: rlog.c,v 1.3 1994/05/11 22:39:44 phk Exp $")
 		continue;
 
 	    if ( versionlist ) {
-		aprintf(out, "%s%s %s\n", vstring, workfilename, Head->num);
+		gettree()l
+		aprintf(out, "%s%s %s\n", vstring, workfilename, tiprev());
 		continue;
 	    }
 
