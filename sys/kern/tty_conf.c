cb97543dca5aa0bd71815429f92028314dcfc14e
ache <ache@FreeBSD.org>
1994-05-30 03:23:14 +0000

    From Bde: 	o Removed final reference to nullmodem().

--- sys/kern/tty_conf.c
+++ sys/kern/tty_conf.c
@@ -38,7 +38,7 @@
  * SUCH DAMAGE.
  *
  *	from: @(#)tty_conf.c	7.6 (Berkeley) 5/9/91
- *	$Id: tty_conf.c,v 1.8 1994/05/04 08:27:09 rgrimes Exp $
+ *	$Id: tty_conf.c,v 1.9 1994/05/30 03:23:14 ache Exp $
  */
 
 #include "param.h"
@@ -85,7 +85,7 @@ struct	linesw linesw[] =
 
 #if NTB > 0
 	tbopen, tbclose, tbread, IE(enodev), tbioctl,
-	tbinput, enodev, nullop, ttstart, nullmodem,	/* 3- TABLDISC */
+	tbinput, enodev, nullop, ttstart, ttymodem,	/* 3- TABLDISC */
 #else
 	IE(enodev), VE(enodev), IE(enodev), IE(enodev), IE(enodev),
 	VE(enodev), IE(enodev), IE(enodev), VE(enodev), IE(enodev),
