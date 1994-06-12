9b65da0c7f95bd517cde2f59658ee652a44e2c51
davidg <davidg@FreeBSD.org>
1994-06-12 04:05:29 +0000

    Changed the policy of setting B_AGE to work properly for our relatively large buffer caches. The old policy assumed that only read-ahead blocks would be cached. This commit actually doesn't change much - since we had already #if 0'd out most of the B_AGE set cases long ago.

--- sys/isofs/isofs_vnops.c
+++ sys/isofs/isofs_vnops.c
@@ -1,5 +1,5 @@
 /*
- *	$Id: isofs_vnops.c,v 1.5 1994/03/25 22:26:22 davidg Exp $
+ *	$Id: isofs_vnops.c,v 1.6 1994/06/12 04:05:29 davidg Exp $
  */
 #include "param.h"
 #include "systm.h"
@@ -165,8 +165,6 @@ isofs_read(vp, uio, ioflag, cred)
 		}
 
 		error = uiomove(bp->b_un.b_addr + on, (int)n, uio);
-		if (n + on == imp->im_bsize || uio->uio_offset == ip->i_size)
-			bp->b_flags |= B_AGE;
 		brelse(bp);
 	} while (error == 0 && uio->uio_resid > 0 && n != 0);
 	return (error);
